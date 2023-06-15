702f9a679acafbfb074d2b787b7c7b372eb8034f
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-06-15 11:29:39 -0100

    enviroment prepared, Sidebar created , routes defined and  installed react-icons

---
+++ components/Sidebar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useMemo } from 'react'
+import { usePathname } from "next/navigation";
+
+interface SidebarProps{
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+  children
+}) =>  {
+  const pathname = usePathname();
+
+  const routes = useMemo(()=>[
+    {
+      label: 'Home',
+      active: pathname !== '/search',
+      href: '/',
+    },
+    {
+      label: 'Search',
+      active: pathname === '/search',
+      href: '/search',
+    }
+  ],[pathname]);
+
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Sidebar
