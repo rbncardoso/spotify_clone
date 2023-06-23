0bfb8552c5f6e34913c6439b6f314a23700773ff
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-06-22 23:46:09 -0100

    Created Library

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from './SidebarItem';
+import Library from './Library';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -44,11 +45,14 @@ const Sidebar: React.FC<SidebarProps> = ({
             ))}
           </div>
         </Box>
-        
+
         <Box className='overflow-y-auto h-full'>
-          Song Library 
+          < Library /> 
         </Box>
       </div>
+      <main className='h-full flex-1 overflow-y-auto py-2'>
+        {children}
+      </main>
     </div>
   )
 }
