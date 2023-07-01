1e56249fc1e1df49018bb6b4538488e46670c93f
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-07-01 12:16:01 -0100

    Header styled and added RxCaretLeft

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from 'next/navigation';
 import React from 'react'
+import {twMerge} from 'tailwind-merge'
+import {RxCaretLeft} from 'react-icons/rx'
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -13,11 +15,18 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
   const router = useRouter();
   const handleLogout = () =>{
-
+    //Handle logout in the future
   };
+
   return (
-    <div>
-      Hello Headers!
+    <div className={twMerge('h-fit bg-gradient-to-b from-emerald-800 p-6', className)}>
+      <div className='w-full mb-4 flex items-center justify-between'>
+        <div className='hidden md:flex gap-x-2 items-center'>
+          <button className='rounded-full bg-black flex items-center justify-center transition hover:opacity-75'>
+            <RxCaretLeft className='text-white' size={35}/>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
